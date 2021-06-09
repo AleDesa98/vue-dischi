69a8e0c330d432f695cc9c5147de18e2c60717ba
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-06-09 16:50:57 +0200

    finito main

---
+++ src/components/Album.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <img :src="item.poster" alt="">
+        <h3>{{ item.title }}</h3>
+        <p>{{ item.author }}</p>
+        <p>{{ item.year }}</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Album",
+    props: [ "item" ]
+}
+</script>
+
+<style lang="scss" scoped>
+    section {
+        background-color: #2D3B46;
+        padding: 20px;
+        height: 100%;
+        cursor: pointer;
+        img {
+            width: 100%;
+        }
+        h3 {
+            color: white;
+            margin: 20px 0;
+            font-size: 26px;
+            text-align: center;
+        }
+        p {
+            color: #607078;
+            text-align: center;
+            margin: 0;
+            font-size: 20px;
+        }
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,50 @@
 <template>
   <main>
-
+      <section class="container">
+          <div class="row">
+              <div v-for="(album, index) in albums" :key="index" class="col-6 col-md-4 col-lg-2">
+                  <Album :item="album" />
+              </div>
+          </div>
+      </section>
   </main>
 </template>
 
 <script>
+import Album from './Album.vue';
+import axios from 'axios';
+
 export default {
-    name: "Main"
+    name: "Main",
+    components: {
+        Album
+    },
+    data: function() {
+        return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            albums: []
+        }
+    },
+    created: function() {
+        axios
+            .get(this.apiUrl)
+            .then(
+                (response) => {
+                    this.albums = response.data.response;
+                }
+            )
+            .catch();
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     main {
         background-color: #1E2E3B;
-        padding: 20px;
-        height: 200px;
+        padding: 50px;
+    }
+
+    main > section > div > div {
+        margin: 20px 0;
     }
 </style>
