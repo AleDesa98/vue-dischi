d921b4cb91ccab3a24744445c62b4575cbcd7342
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-06-09 15:52:03 +0200

    finito header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <a href="">
+            <img src="../assets/img/spotify.png" alt="">
+        </a>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        display: flex;
+        align-items: center;
+        background-color: #2D3B46;
+        height: 70px;
+        a {
+            margin-left: 20px;
+            img {
+                height: 50px;
+            }
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: "Main"
+}
+</script>
+
+<style lang="scss" scoped>
+    main {
+        background-color: #1E2E3B;
+        padding: 20px;
+        height: 200px;
+    }
+</style>
