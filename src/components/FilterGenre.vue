c86014447108944580cd23c7f4c5f31fab454eeb
AleDesa98 <alessandrodesantis1998@gmail.com>
2021-06-10 17:07:43 +0200

    finito filter

---
+++ src/components/FilterGenre.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <select v-model="filterGenre" @change="$emit('performeFilter', filterGenre)" name="genrename" id="genreid">
+            <option value="">Seleziona un genere</option>
+            <option value="Rock">Rock</option>
+            <option value="Pop">Pop</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
+        </select>
+    </form>
+</template>
+
+<script>
+export default {
+    name:"FilterGenre",
+    data: function() {
+        return {
+            filterGenre: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,13 @@
 <template>
   <main>
       <section class="container">
+
+          <div class="col-12 text-center">
+              <FilterGenre @performeFilter="filterAlbum" />
+          </div>
+
           <div class="row">
-              <div v-for="(album, index) in albums" :key="index" class="col-6 col-md-4 col-lg-2">
+              <div v-for="(album, index) in filteredAlbums" :key="index" class="col-6 col-md-4 col-lg-2">
                   <Album :item="album" />
               </div>
           </div>
@@ -13,16 +18,34 @@
 <script>
 import Album from './Album.vue';
 import axios from 'axios';
+import FilterGenre from './FilterGenre.vue'
 
 export default {
     name: "Main",
     components: {
-        Album
+        Album,
+        FilterGenre
     },
     data: function() {
         return {
             apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
-            albums: []
+            albums: [],
+            selectedGenre: ''
+        }
+    },
+    computed: {
+        filteredAlbums: function() {
+            const newArray = this.albums.filter(
+                (element) => {
+                    return element.genre.includes(this.selectedGenre)
+                }
+            );
+            return newArray;
+        }
+    },
+    methods: {
+        filterAlbum: function(filter) {
+            this.selectedGenre = filter;
         }
     },
     created: function() {
